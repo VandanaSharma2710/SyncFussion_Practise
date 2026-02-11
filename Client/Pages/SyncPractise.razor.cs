3380937fb9d75bf0ce7236bb59a9381bfa034d5a
VandanaSharma2710 <sharmavandana657@gmail.com>
2026-02-11 18:59:23 +0530

    Add input boxes

--- Client/Pages/SyncPractise.razor.cs
+++ Client/Pages/SyncPractise.razor.cs
@@ -6,6 +6,13 @@ namespace Client.Pages
         
         private string message = "";
         private bool isLoading = false;
+       
+
+        public String Name = "";
+        public int Age = 0;
+        public String Game = "";
+        public bool IsActive = false;
+
         private void onClick()
         {
             message = "Button clicked!";
@@ -20,14 +27,32 @@ namespace Client.Pages
 
         public List<Player> playerList = new()
         {
-            new Player{Id=1, Name="Sachin", Game="Cricketer"},
-            new Player{Id=2, Name="Gopichand", Game="Hockey"},
-            new Player{Id=3, Name="Sania", Game="Tanish"},
-            new Player{Id=3, Name="PT Usha", Game="Atheletics"},
+            new Player{Id=1, Name="Sachin", Age =45, Game="Cricketer",IsActive=false},
+            new Player{Id=2, Name="Gopichand", Age =47, Game="Hockey",IsActive=false},
+            new Player{Id=3, Name="Sania",Age =35, Game="Tanish", IsActive = false},
+            new Player{Id=3, Name="PT Usha", Age =45,Game="Atheletics", IsActive = false},
 
 
         };
 
+        private void saveInfo()
+        {
+            var newPlayer = new Player
+            {
+                Id = playerList.Count + 1,
+                Name= Name,
+                Age = Age,
+                Game = Game,
+                IsActive = IsActive
+            };
+            playerList.Add(newPlayer);
+
+            //clear the form
+            Name = "";
+             Age = 0;
+             Game = "";
+             IsActive = false;  
+        }
     }
 
     public class Player
@@ -35,6 +60,12 @@ namespace Client.Pages
         public int Id { get; set; }
 
         public String Name { get; set; }
+
+        public int Age { get; set; }
         public String Game { get; set; }
+
+        public bool IsActive { get; set; }
+
+        
     }
 }
